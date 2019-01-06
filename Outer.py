42de0a63ba6e13c2c3e74b44f09b1db0a1a776e0
Gzb <gaspardzebear@hotmail.fr>
2019-01-06 16:08:23 +0100

    SQLprocessor work. Tablesorter jquery for dataframe

--- Outer.py
+++ Outer.py
@@ -105,13 +105,27 @@ class OutputHtml(Out) :
         list-style-type: none;
     }
     </style>
-    </head>
+   <script
+  src="https://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"></script>
+    <script
+  src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"
+  integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU="
+  crossorigin="anonymous"></script>
+<script src="https://mottie.github.io/tablesorter/dist/js/jquery.tablesorter.min.js"></script>
+  <script>
+$(document).ready((function() {
+  $(".dataframe").tablesorter();
+}));
+  </script>
+</head>
+
     <body>
     <a id="top"></a>
     """
 
     self.head=head
-    logging.warning(head)
 
   #--------------------------------------------------------------------------------------
   def close(self) :
--- SQLProcessor.py
+++ SQLProcessor.py
@@ -29,7 +29,7 @@ class SQLFramor() :
     self.remove={"Database and Connection Pool":"Sql","Acqu Time/Trans [ms]":"AcqTime","Executions/Trans":"ExecPerTrans",
       "Exec Total [ms]":"ExecTotal","Exec Avg [ms]":"ExecAvg","Exec Max [ms]":"ExecMax","Exec Min [ms]":"ExecMin","Executions": "ExecCount",
       "Failed %":"PFailed","Round trips":"RT"}
-    self.drop=["ExecPerTrans","RT"]
+    self.drop=["RT"]
     SQLFramor.fileNum += 1
     SQLFramor.databaseNum  = 0 
     SQLFramor.sqlNum  = 0 
@@ -93,19 +93,30 @@ class SQLComparator() :
   #--------------------------------------------------------------------------------------
   def go(self) :
     dfm=pd.merge(self.df1,self.df2,on='SqlHash',how='outer')
-    dfm.fillna(0)
+    #dfm.fillna(0)
     dfm['DeltaCount']=dfm.apply(lambda x: x['ExecCount_y'] - x['ExecCount_x'],axis=1 ) 
     dfm['DeltaCountPercent']=dfm.apply(SQLComparator.deltaCountPercent,axis=1 ) 
-    dfm=dfm[(dfm['ExecCount_x'] > self.p['autofocuscount']) & (dfm['ExecCount_y'] > self.p['autofocuscount']) ]
-    dfm=dfm[(dfm['ExecAvg_x'] > self.p['autofocusmean']) & (dfm['ExecAvg_y'] > self.p['autofocusmean']) ]
-    dfm=dfm[(abs(dfm['DeltaCountPercent']) > 10)]
-    if dfm.size == 0 :
-      return
+    dfm['DeltaExecPerTrans']=dfm.apply(lambda x: x['ExecPerTrans_y'] - x['ExecPerTrans_x'],axis=1 ) 
     dfm['xLnk']=dfm.apply(lambda x: "<a href=\"#" + str(x['SqlId_x']) + "\">" + str(x['SqlId_x']) + "</a>" ,axis=1 )
     dfm['yLnk']=dfm.apply(lambda x: "<a href=\"#" + str(x['SqlId_y']) + "\">" + str(x['SqlId_y']) + "</a>" ,axis=1 )
-    self.p['out'].h2("Comparison of requests")
+    filter=False
+    if filter :
+      dfm=dfm[(dfm['ExecCount_x'] > self.p['autofocuscount']) & (dfm['ExecCount_y'] > self.p['autofocuscount']) ]
+      dfm=dfm[(dfm['ExecAvg_x'] > self.p['autofocusmean']) & (dfm['ExecAvg_y'] > self.p['autofocusmean']) ]
+      dfm=dfm[(abs(dfm['DeltaCountPercent']) > 10)]
+    logging.warning(dfm)
+    if dfm.size == 0 :
+      return
+    dfm1=dfm[dfm['Sql_x'].notnull() & dfm['Sql_y'].isnull()]
+    dfm2=dfm[dfm['Sql_x'].isnull() & dfm['Sql_y'].notnull()]
+    dfm=dfm[dfm['Sql_x'].notnull() & dfm['Sql_y'].notnull()]
     with pd.option_context('display.max_rows', None) :
-      self.p['out'].out("Compare",dfm[['xLnk','Sql_x','yLnk','Sql_y','ExecCount_x','ExecCount_y','DeltaCount','DeltaCountPercent','ExecAvg_x','ExecAvg_y']],False)
+      self.p['out'].h2("Comparison of requests")
+      self.p['out'].out("Compare",dfm[['xLnk','yLnk','Sql_x','ExecCount_x','ExecCount_y','DeltaCount','DeltaCountPercent','ExecAvg_x','ExecAvg_y','ExecPerTrans_x','ExecPerTrans_y']],False)
+      self.p['out'].h2("Request in file1 only")
+      self.p['out'].out("Compare",dfm1[['xLnk','Sql_x','ExecCount_x','ExecAvg_x','ExecPerTrans_x']],False)
+      self.p['out'].h2("Request in file2 only")
+      self.p['out'].out("Compare",dfm2[['yLnk','Sql_y','ExecCount_y','ExecAvg_y','ExecPerTrans_y']],False)
 
 
 #--------------------------------------------------------------------------------------
@@ -115,6 +126,7 @@ class SQLProcessor() :
   def __init__(self,param) :
     self.param=param
     self.p=self.param.getAll()
+    self.p['out'].h1('SQLProcessor compare ' + self.p['file1'] + ' and ' + self.p['file2'])
     self.df1=SQLFramor(self.param,self.p['file1']).getRawdatas()
     self.df2=SQLFramor(self.param,self.p['file2']).getRawdatas()
 
